dd768f9f0501ed4570a06e04d20a42ffbc6be1cb
Leo <wx1994c@gmail.com>
2018-05-12 03:12:40 -0700

    part2

--- app/src/main/java/edu/washington/wanxic/arewethereyet/Alarm.kt
+++ app/src/main/java/edu/washington/wanxic/arewethereyet/Alarm.kt
@@ -4,17 +4,23 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.media.MediaPlayer
+import android.telephony.SmsManager
 import android.util.Log
 import android.widget.Toast
+import android.app.PendingIntent
+import android.app.AlarmManager
 
 
 class Alarm : BroadcastReceiver() {
     override fun onReceive(context: Context?, intent: Intent?) {
+        val smsManager = SmsManager.getDefault()
+
         val message = intent!!.getStringExtra("message")
         val phone = intent!!.getStringExtra("phone")
         val phoneNum = "(" + phone.substring(0, 3) + ")" + phone.substring(3, 6) + "-" + phone.substring(6)
 
-        Toast.makeText(context, "$phoneNum : $message", Toast.LENGTH_SHORT).show()
-
+        smsManager.sendTextMessage(phone, null, message, null, null)
+        Toast.makeText(context, "$phoneNum: $message", Toast.LENGTH_SHORT).show()
+        Log.i("MainActivity", "repeat")
     }
 }
